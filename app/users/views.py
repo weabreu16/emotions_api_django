9a5658a3453ae80e3f875ab15a8181f07dae95f4
William Abreu <weabreu16@gmail.com>
2022-12-17 21:42:40 -0400

    feat(users): get active users instead of all users

--- app/users/views.py
+++ app/users/views.py
@@ -1,7 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 from .serializers import UserSerializer
 from .models import User
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def list(self, request, *args, **kwargs):
+        users = self.get_queryset().filter(is_active=True)
+        serializer = self.get_serializer(users, many=True)
+        return Response(serializer.data)
