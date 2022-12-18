ce6652477639f47426238c98296351113a61a2d1
William Abreu <weabreu16@gmail.com>
2022-12-17 22:11:51 -0400

    feat(users): email field is unique

--- app/users/models.py
+++ app/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    email = models.EmailField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
     password = models.CharField(max_length=255)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
