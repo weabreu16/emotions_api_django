ed6e22486b023dd1923bcd8a64784a34fcb388da
William Abreu <weabreu16@gmail.com>
2022-12-17 22:04:22 -0400

    feat(users): create users endpoint

--- app/users/views.py
+++ app/users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 from .serializers import UserSerializer
@@ -11,3 +12,17 @@ class UserViewSet(viewsets.ModelViewSet):
         users = self.get_queryset().filter(is_active=True)
         serializer = self.get_serializer(users, many=True)
         return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        user = request.data
+
+        if user.get('password'):
+            user['password'] = make_password(user['password'])
+
+        serializer = self.get_serializer(data=user)
+        serializer.is_valid(raise_exception=True)
+
+        self.perform_create(serializer)
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
