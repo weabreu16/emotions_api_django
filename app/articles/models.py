6bf5225873e4e7f34528954f517419d769eba221
William Abreu <weabreu16@gmail.com>
2022-12-18 22:14:15 -0400

    feat(articles): article model

--- app/articles/models.py
+++ app/articles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    psychologist = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
