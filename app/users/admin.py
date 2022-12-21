cb9355ff269c716524c5d7a05e9cc5206c379fe5
William Abreu <weabreu16@gmail.com>
2022-12-20 22:40:35 -0400

    feat(users): display and search bar in users admin page

--- app/users/admin.py
+++ app/users/admin.py
@@ -3,5 +3,7 @@ from .forms import User, UserForm
 
 class UserAdmin(admin.ModelAdmin):
     form = UserForm
+    list_display = ['email', 'first_name', 'last_name', 'is_active', 'created_at']
+    search_fields = ['email', 'first_name', 'last_name']
 
 admin.site.register(User, UserAdmin)
