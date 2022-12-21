ae412220c251698287818ebf338291b9fd3b4ca9
William Abreu <weabreu16@gmail.com>
2022-12-20 20:28:12 -0400

    feat(articles): implement eager loading mixin to get article serializer

--- app/articles/serializers.py
+++ app/articles/serializers.py
@@ -2,9 +2,11 @@ from rest_framework import serializers
 from .models import Article
 from users.models import User
 from users.serializers import UserSerializer
+from common.mixins import EagerLoadingMixin
 
-class GetArticleSerializer(serializers.ModelSerializer):
+class GetArticleSerializer(EagerLoadingMixin, serializers.ModelSerializer):
     psychologist = UserSerializer()
+    select_related_fields = ['psychologist']
 
     class Meta:
         model = Article
