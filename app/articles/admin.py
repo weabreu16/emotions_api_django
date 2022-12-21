afed49fc8584281771b14091c566bb62d27972d4
William Abreu <weabreu16@gmail.com>
2022-12-20 22:36:44 -0400

    feat(articles): articles admin page

--- app/articles/admin.py
+++ app/articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'psychologist', 'is_active', 'created_at']
+    list_select_related = ['psychologist']
+    raw_id_fields = ['psychologist']
+    search_fields = ['title', 'psychologist__email', 'psychologist__first_name', 'psychologist__last_name']
+
+admin.site.register(Article, ArticleAdmin)
