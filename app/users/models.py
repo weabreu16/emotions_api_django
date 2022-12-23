37876d9fd43fbdbbfcd318c697c55f359d763bd3
William Abreu <weabreu16@gmail.com>
2022-12-23 14:55:10 -0400

    feat(users): note model

--- app/users/models.py
+++ app/users/models.py
@@ -8,3 +8,13 @@ class User(models.Model):
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Note(models.Model):
+    psychologist = models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True, related_name="psychologist")
+    patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="patient")
+    note = models.TextField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['psychologist','patient'], name="unique_psychologist_patient")
+        ]
