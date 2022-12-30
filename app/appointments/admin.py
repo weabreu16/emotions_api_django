c521c3d56eef484b438e8938dacbc6ccde6fc913
William Abreu <weabreu16@gmail.com>
2022-12-30 13:51:36 -0400

    feat(appointments): admin site

--- app/appointments/admin.py
+++ app/appointments/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Appointment
 
-# Register your models here.
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ['description', 'psychologist', 'patient', 'start', 'end', 'status', 'is_active', 'created_at']
+    list_select_related = ['psychologist', 'patient']
+    raw_id_fields = ['psychologist', 'patient']
+    search_fields = [
+        'psychologist__email', 
+        'psychologist__first_name', 
+        'psychologist__last_name',
+        'patient__email', 
+        'patient__first_name', 
+        'patient__last_name',
+    ]
+
+admin.site.register(Appointment, AppointmentAdmin)
