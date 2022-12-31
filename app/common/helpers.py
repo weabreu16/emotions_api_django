1a5c6a61f838a911abf58b4db3153c2951d23cf7
William Abreu <weabreu16@gmail.com>
2022-12-31 01:01:18 -0400

    feat(common): callable type in cast and raise exception flag

--- app/common/helpers.py
+++ app/common/helpers.py
@@ -1,10 +1,16 @@
-from typing import TypeVar
+from typing import Callable, TypeVar
 from django.http import QueryDict
 from rest_framework.exceptions import ParseError
 
 T = TypeVar('T')
 
-def get_param(query_params: QueryDict, param: str, default: T = None, cast: T = None):
+def get_param(
+        query_params: QueryDict, 
+        param: str, 
+        default: T = None, 
+        cast: T | Callable[[str], T] = None,
+        raise_exception: bool = True
+    ) -> T | str:
     """
     Get the `param` value inside a `QueryDict`.
 
@@ -15,14 +21,17 @@ def get_param(query_params: QueryDict, param: str, default: T = None, cast: T = 
         Name or key of the param in the query string.
 
     @default: T, optional
-        Value that will return if param does not exist, if None, it will raise `ParseError`.
+        Value that will return if param does not exist. Default to True.
     
-    @cast: T, optional
+    @cast: T | (str) -> T, optional
         Function that will transform the value. If error occur, it will raise `ParseError`. 
+
+    @raise_exception: bool, optional
+        If true, it will raise exception is None is returned. Default to True.
     """
     value = query_params.get(param, default)
 
-    if not value:
+    if not value and raise_exception:
         raise ParseError(f"{param} does not exist")
     
     if cast:
